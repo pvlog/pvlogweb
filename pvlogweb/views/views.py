a07a4f6e539281fbce0aa41ca4911bbd1b665c5f
Benjamin <benjaminst123@gmail.com>
2016-11-19 17:27:02 +0100

    Overview + events handling

--- pvlogweb/views/views.py
+++ pvlogweb/views/views.py
@@ -1,5 +1,12 @@
 from flask.templating import render_template
 from pvlogweb import app
+import requests
+from util import CommunicationError
+import json
+
+url = "http://192.168.178.82:8383"
+headers = {'content-type': 'application/json'}
+
 
 @app.route('/about')
 def about():
@@ -7,3 +14,24 @@ def about():
     :return: about page
     """
     return render_template("about.html")
+
+@app.route('/events')
+def events():
+    """Render event page
+    :return: event page
+    """
+
+    payload = {
+        "method": "getEvents",
+        "params": {},
+        "jsonrpc": "2.0",
+        "id": 0
+    }
+
+    response = requests.post(
+        url, data=json.dumps(payload), headers=headers).json()
+
+    if (response["jsonrpc"] != "2.0") or (response["id"] != 0):
+        raise CommunicationError("getEvents")
+
+    return render_template("events.html", data=response["result"])
