cc028b257a06a68850f7244e6bb41f860ba09edc
Benjamin <benjaminst123@gmail.com>
2017-05-08 16:07:59 +0200

    Add default config

---
+++ pvlogweb/config.py
@@ -0,0 +1,24 @@
+DEBUG = False
+
+TESTING = False
+
+WEBPACK_MANIFEST_PATH = 'manifest.json'
+
+SESSION_TYPE = 'filesystem'
+
+SESSION_FILE_DIR = '/tmp'
+
+BABEL_DEFAULT_LOCALE='en'
+
+LANGUAGES = {
+        'en': 'English',
+        'de': 'Deutsch'
+}
+
+STATIC_FOLDER = None
+
+PASSWORD_FILE = '/var/lib/pvlogweb/password.conf'
+
+PVLOG_SERVER = "http://localhost:8383"
+ 
+PVLOG_ADMIN_SERVER = "http://localhost:8384"
